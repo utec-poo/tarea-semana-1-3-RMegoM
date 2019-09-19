1f6551a20b227e210c5f3ce16bc61ad94611f1f4
RMegoM <55498790+RMegoM@users.noreply.github.com>
2019-09-18 22:02:45 -0500

    Update main.cpp

--- ejercicio7/main.cpp
+++ ejercicio7/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "Tipos.h"
+#include <string>
 using namespace std;
 
 int main() {
+  int x;
+  cout<<"número: ";
+  cin>>x;
+  int lista[x];
+  if(10<=x || x<=30){
+    for(int i=0;i<x;i++){
+      cout<<"*";
+    }
+  }
+  else{
+    cout<<"Erro";
+  }
 
-entero numero;
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+return 0;
 }
