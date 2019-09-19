3b2ae16b5cd0a789bf9d4a11d9e574bf079fd045
RMegoM <55498790+RMegoM@users.noreply.github.com>
2019-09-18 22:56:46 -0500

    Update main.cpp

--- ejercicio10/main.cpp
+++ ejercicio10/main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include "Tipos.h"
+#include <string>
 using namespace std;
-
+int invertir(int , int, int);
 int main() {
+  int lista[10]={900464009, 914103031, 925979529 , 935935606, 963839025 ,966173734, 978119539, 982727289, 989950857, 999958817};
+  int numinv=0;
+  int res;
+  int n;
 
-entero numero;
-caracter crt;
+  for(int i=0;i<10;i++){
+    if(invertir(lista[i],numinv, res)==lista[i]){
+      cout<<lista[i]<<" ";
+    }
+  }
 
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+}
+int invertir(int n, int numinv, int res){
+    while(n>0){
+    res=n%10;
+    n=n/10;
+    numinv=numinv*10+res;
+  }
+  return numinv;
 }
