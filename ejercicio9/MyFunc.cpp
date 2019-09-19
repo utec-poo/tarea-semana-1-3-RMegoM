3e36a382b5b881a01c38763fb087d6624b36ce28
RMegoM <55498790+RMegoM@users.noreply.github.com>
2019-09-18 22:26:25 -0500

    Update MyFunc.cpp

--- ejercicio9/MyFunc.cpp
+++ ejercicio9/MyFunc.cpp
@@ -1,10 +1,28 @@
 #include "MyFunc.h"
 
-
-int invertir(int n){
-// Escriba su código aqui
-}
-
-bool es_capicua(int n){
-// Escriba su código aqu
+int invertir(long int n, int hora, int minuto, int segundo){
+  long int n;
+  //calculamos la hora//
+  if(n/3600>0){
+    hora=a;
+  }
+  else{
+    hora=00;
+  }
+  //calculamos los minutos//
+  if(int (n-hora*3600)/60 >0){
+    minuto=(n-hora*3600)/60;
+  }
+  else{
+    minuto=00;
+  }
+  //calculamos los segundos//
+  int c=(n-hora*3600-minuto*60);
+  if(int (n-hora*3600-minuto*60) >0){
+    segundo=(n-hora*3600-minuto*60);
+  }
+  else{
+    segundo=00;
+  }
+  cout<<hora<<":"<<minuto<<":"<<segundo;
 }
